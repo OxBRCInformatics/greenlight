21d1fd0335dfc6db772e12dd5797113375425cc5
Ryan Brooks <ryanbrooksis@gmail.com>
2014-03-13 10:04:45 +0000

    Adding Geb config and travis build

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -4,13 +4,19 @@
 	See: http://www.gebish.org/manual/current/configuration.html
 */
 
+
+import geb.driver.SauceLabsDriverFactory
+import org.openqa.selenium.Platform
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
 reportsDir = new File("target/geb-reports")
 reportOnTestFailureOnly = false
 
-
+projectName = "greenlight"
+seleniumVersion = "2.39.0"
 
 driver = {
     new FirefoxDriver()
@@ -47,5 +53,38 @@ environments {
     firefox {
         driver = { new FirefoxDriver() }
     }
+	sauceIe7 {
+		String username = System.getenv("SAUCE_USERNAME");
+		String apiKey = System.getenv("SAUCE_ACCESS_KEY");
+		if(username == null || apiKey == null){
+			throw new IllegalArgumentException("Sauce OnDemand credentials not set.")
+		}
+		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+		caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
+		caps.setCapability("selenium-version", seleniumVersion);
+		caps.setCapability("name", projectName);
+		caps.setCapability("platform", Platform.XP);
+		caps.setCapability("version", "7");
 
+
+		driver = {
+			new RemoteWebDriver(new URL("http://${username}:${apiKey}@ondemand.saucelabs.com:80/wd/hub"), caps)
+		}
+	}
+	sauceChrome{
+		String username = System.getenv("SAUCE_USERNAME");
+		String apiKey = System.getenv("SAUCE_ACCESS_KEY");
+		if(username == null || apiKey == null){
+			throw new IllegalArgumentException("Sauce OnDemand credentials not set.")
+		}
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
+		caps.setCapability("selenium-version", seleniumVersion);
+		caps.setCapability("name", projectName);
+		caps.setCapability("platform", Platform.LINUX);
+
+		driver = {
+			new RemoteWebDriver(new URL("http://${username}:${apiKey}@ondemand.saucelabs.com:80/wd/hub"), caps)
+		}
+	}
 }
