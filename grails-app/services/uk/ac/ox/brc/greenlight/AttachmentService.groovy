f504e4adb18ed2731e9831ec81c89282ba797598
Ryan Brooks <ryanbrooksis@gmail.com>
2014-03-31 10:22:07 +0100

    Cleaning commented-out code

--- grails-app/services/uk/ac/ox/brc/greenlight/AttachmentService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/AttachmentService.groovy
@@ -1,39 +1,21 @@
 package uk.ac.ox.brc.greenlight
 
 import grails.transaction.Transactional
-import org.hibernate.criterion.CriteriaSpecification
 
 @Transactional
 class AttachmentService {
 
-
     def getAllAttachments() {
       Attachment.list(sort: 'dateOfUpload', order: 'desc');
-//        def result=
-//        Attachment.createCriteria().list() {
-//            resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
-//            projections {
-//                property 'dateOfUpload'
-//                property 'attachmentType'
-//                property 'fileName'
-//                property 'id'
-//            }
-//            sort: 'dateOfUpload'
-//            order: 'desc'
-//        }
 
         //Fixme try to use projection and
         //do not load Content image files
         //add a Boolean loadContent parameter to this method
     }
 
-
-
-
     def save(Attachment attachment) {
         attachment.save(flush: true);
     }
-
 
     byte[] getContent(def id) {
         def attachment = Attachment.get(id)
@@ -42,6 +24,5 @@ class AttachmentService {
             content = attachment?.content;
         return content
     }
-
 }
 
