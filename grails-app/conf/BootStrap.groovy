a20764c932f91c6ca244ae54c65901c795715094
Ryan Brooks <ryanbrooksis@gmail.com>
2014-04-01 16:28:25 +0100

    Updating Bootstrap so the correct items are optional on ORB forms.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -49,17 +49,17 @@ class BootStrap {
                     name: "ORB General Consent Form",
                     namePrefix: "GEN",
                     templateVersion: "v1 October 2013"
-            ).addToQuestions(new Question(name: 'I have read and understood the information sheet for this study (Version 1 dated December 2013). I have had the opportunity to ask questions and have had these answered satisfactorily.')
+            ).addToQuestions(new Question(optional: true, name: 'I have read and understood the information sheet for this study (Version 1 dated December 2013). I have had the opportunity to ask questions and have had these answered satisfactorily.')
             ).addToQuestions(new Question(name: 'I agree to give samples for research and/or allow samples already collected as part of my medical care to be used by the biobank.')
             ).addToQuestions(new Question(name: 'I agree that further blood and/or tissue samples may be taken for the biobank during the course of my hospital care.  I understand that I will be asked for permission each time.')
             ).addToQuestions(new Question(name: 'I understand that my participation is voluntary and that I am free at any time to withdraw my permission for the storage and distribution of any of my samples that have not already been used in research.  Withdrawing from the biobank will not affect my present and future medical care and legal rights in any way.')
             ).addToQuestions(new Question(name: 'I agree that biobank staff can collect and store information from my health care records for research that uses my samples.  I understand that the biobank will keep my information confidential. Information will only be passed on to researchers in a form that protects my identity.')
-            ).addToQuestions(new Question(name: 'I understand that results from research tests on my samples might be medically important to me.  I agree to my hospital consultant and GP being informed, and that research findings that are important for treating serious medical conditions I may have can be discussed with me.')
+            ).addToQuestions(new Question(optional: true, name: 'I understand that results from research tests on my samples might be medically important to me.  I agree to my hospital consultant and GP being informed, and that research findings that are important for treating serious medical conditions I may have can be discussed with me.')
             ).addToQuestions(new Question(name: 'I understand and agree that my samples will be considered a gift to the University of Oxford. If a commercial product were developed as a result of research in which my sample was used, I would not profit financially.  ')
             ).addToQuestions(new Question(name: 'I give permission for the biobank to store my samples and distribute them for use in any medical research that has research ethics committee approval. I understand that future laboratory research may use new tests or techniques that are not yet known.')
             ).addToQuestions(new Question(name: 'I understand that relevant sections of my medical notes and data collected by the biobank may be looked at by authorised individuals from The University of Oxford, NHS organisations, funding agencies and research governance monitors. I permit these individuals to access my research records.')
             ).addToQuestions(new Question(name: 'Genetic research: I understand and agree that my samples may be used in genetic research aimed at understanding the genetic influences on disease and that the results of these investigations are unlikely to have any implications for me personally.')
-            ).addToQuestions(new Question(name: 'I agree to be contacted about ethically approved research studies for which I may be suitable. I understand that agreeing to be contacted does not oblige me to participate in any further studies.')
+            ).addToQuestions(new Question(optional: true, name: 'I agree to be contacted about ethically approved research studies for which I may be suitable. I understand that agreeing to be contacted does not oblige me to participate in any further studies.')
             ).save(failOnError: true)
         }
 
@@ -72,7 +72,7 @@ class BootStrap {
                     name: "ORB Specific Programme Clinically Relevant Genomics - Oncology Consent Form for Adults",
                     namePrefix: "CRA",
                     templateVersion: "v1 October 2013"
-            ).addToQuestions(new Question(name: 'I have read and understood the information sheet for this study (Version 1 dated October 2013). I have had the opportunity to ask questions and have had these answered satisfactorily.')
+            ).addToQuestions(new Question(optional: true, name: 'I have read and understood the information sheet for this study (Version 1 dated October 2013). I have had the opportunity to ask questions and have had these answered satisfactorily.')
             ).addToQuestions(new Question(name: 'I agree to give samples for research and/or allow samples already collected as part of my medical care to be used by the biobank.')
             ).addToQuestions(new Question(name: 'I agree that further blood and/or tissue samples may be taken for the biobank during the course of my hospital care.  I understand that I will be asked for permission each time.')
             ).addToQuestions(new Question(name: 'I understand that my participation is voluntary and that I am free at any time to withdraw my permission for the storage and distribution of any of my samples that have not already been used in research.  Withdrawing from the biobank will not affect my present and future medical care and legal rights in any way.')
@@ -81,36 +81,11 @@ class BootStrap {
             ).addToQuestions(new Question(name: 'I give permission for the biobank to store my samples and distribute them for use in any medical research that has research ethics committee approval. I understand that future laboratory research may use new tests or techniques that are not yet known.')
             ).addToQuestions(new Question(name: 'I understand that relevant sections of my medical notes and data collected by the biobank may be looked at by authorised individuals from The University of Oxford, NHS organisations, funding agencies and research governance monitors. I permit these individuals to access my research records.')
             ).addToQuestions(new Question(name: 'Genetic research: I understand and agree that my samples may be used in genetic research aimed at understanding the genetic influences on disease.')
-            ).addToQuestions(new Question(name: 'Clinically relevant results: I agree that findings from genetic and other testing related to the reason I am currently undergoing investigations will be fed back to my clinician so that they may be used in decisions about my treatment.')
-            ).addToQuestions(new Question(name: 'Incidental findings: I understand and agree that I will be informed of any results of genetic analysis of my sample where they are NOT relevant to the condition being investigated, but are judged to be important for my/my family’s health care, and can be acted upon medically.')
-            ).addToQuestions(new Question(name: 'I agree to be contacted about ethically approved research studies for which I may be suitable. I understand that agreeing to be contacted does not oblige me to participate in any further studies.')
+            ).addToQuestions(new Question(optional: true, name: 'Clinically relevant results: I agree that findings from genetic and other testing related to the reason I am currently undergoing investigations will be fed back to my clinician so that they may be used in decisions about my treatment.')
+            ).addToQuestions(new Question(optional: true, name: 'Incidental findings: I understand and agree that I will be informed of any results of genetic analysis of my sample where they are NOT relevant to the condition being investigated, but are judged to be important for my/my family’s health care, and can be acted upon medically.')
+            ).addToQuestions(new Question(optional: true, name: 'I agree to be contacted about ethically approved research studies for which I may be suitable. I understand that agreeing to be contacted does not oblige me to participate in any further studies.')
             ).save(failOnError: true)
         }
-
-
-
-
-//
-//        if(ConsentFormTemplate.count()==1)
-//        {
-//            new ConsentFormTemplate(
-//                    name: "ORB General Consent Form",
-//                    namePrefix: "GEN",
-//                    templateVersion: "v1 October 2013"
-//            ).addToQuestions(new Question(name: 'I have read and understood the patient information sheet (green v1.2 dated 3rd of March 2009). My questions have been answered satisfactorily. I know how to contact the research team')
-//            ).addToQuestions(new Question(name: 'I agree to give a sample of blood and/or other tissues for research.')
-//            ).addToQuestions(new Question(name: 'I agree that further blood and/or tissue samples may be taken for the research during the course of my hospital care. I understand that I will be asked for permission each time.')
-//            ).addToQuestions(new Question(name: 'I understand how the samples will be taken, that participation is voluntary and that I am free at any time to withdraw my permission for the storage and distribution of my samples providing they have not already been used in research.')
-//            ).addToQuestions(new Question(name: 'I agree that the biobank staff can collect and store information from my health care records for research that used my samples. I understand the biobank will keep my information confidential. Information will only be passed to researchers in an anonymous way that protects my identity.')
-//            ).addToQuestions(new Question(name: 'I understand results from research tests on my samples might be medically important to me. I agree to my hospital consultant and GP being informed and that relevant experimental findings can be discussed with me. ')
-//            ).addToQuestions(new Question(name: 'I agree to gift blood samples taken for the purpose of the research to the University of Oxford. if a commercial product were developed as a result of the study, I will not profit financially from such a product')
-//            ).addToQuestions(new Question(name:'I give permission for the biobank to store my samples and distribute them for use in any medical research that has research ethics committee approval. I understand that future laboratory research may use new tests or techniques that are not yet known.')
-//            ).addToQuestions(new Question(name:'Consent for genetic research: I understand that my samples my be used un  genetic research aimed at understanding the genetic influences on diseases and that results of these investigations are unlikely to have an implications for me personally.')
-//            ).addToQuestions(new Question(name:'I understand that relevant sections of my medical notes and data collected by ORB, may be looked at by individuals from Oxford University, from regulatory authorities or from the NHS Trust, where it is relevant to my taking part in this research. I give permission for these individuals to have access to my records.')
-//            ).save(failOnError: true)
-//        }
-
-
 
 //        new ConsentFormTemplate(
 //                name: "ORB Verbal Consent Form",
