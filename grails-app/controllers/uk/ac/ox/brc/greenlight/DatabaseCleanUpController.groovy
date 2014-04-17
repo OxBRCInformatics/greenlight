efdb4aae5785285ce9c1436548f1823382bf61d1
soheilsaif <s.saifipour@gmail.com>
2014-04-17 16:27:45 +0100

    Fixes #69, fixing deletion issue

---
+++ grails-app/controllers/uk/ac/ox/brc/greenlight/DatabaseCleanUpController.groovy
@@ -0,0 +1,35 @@
+package uk.ac.ox.brc.greenlight
+
+import grails.transaction.Transactional
+
+class DatabaseCleanUpController {
+
+	def databaseCleanupService
+
+	def cleanOrphanResponses()
+	{
+		def responseStr = getResponsesStatusStr()
+
+		try {
+			databaseCleanupService.cleanOrphanResponses()
+		}
+		catch (Exception exception)
+		{
+			render exception.message
+			return
+		}
+
+		responseStr = responseStr + "<br>"+ getResponsesStatusStr()
+		render responseStr
+	}
+
+	private def getResponsesStatusStr()
+	{
+		def allResponses = Response.count()
+		def allConsentResponses = 0
+		ConsentForm.list().each { consent->
+			allConsentResponses += consent.responses.size()
+		}
+		return "AllResponses=${allResponses}   allConsentResponses=${allConsentResponses}"
+	}
+}
