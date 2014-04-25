3521b72637418343bce93ef537a53e826055e1e0
Ryan Brooks <ryanbrooksis@gmail.com>
2014-04-25 09:14:53 +0100

    Adding database image migration service. Working on #54

--- grails-app/services/uk/ac/ox/brc/greenlight/AttachmentService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/AttachmentService.groovy
@@ -1,6 +1,7 @@
 package uk.ac.ox.brc.greenlight
 
 import grails.transaction.Transactional
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
 
 @Transactional
@@ -28,9 +29,9 @@ class  AttachmentService {
 		attachment.save()
 
 		if (attachment.validate()) {
-			String filePath = fileUploadService.uploadFile(uploadedFile, attachment.id + ".jpg", 'attachments')
-			attachment.fileUrl = filePath
-			attachment.save()
+				String filePath = fileUploadService.uploadFile(uploadedFile, attachment.id + ".jpg", 'attachments')
+				attachment.fileUrl = filePath
+				attachment.save()
 		}
 		else {
 			log.error("Saving attachment failed: " + attachment.errors)
@@ -49,5 +50,47 @@ class  AttachmentService {
             content = attachment?.content;
         return content
     }
+
+	/**
+	 * Move the contents of the Attachment from the database into the filesystem. If there is no content in the `content`
+	 * attribute then no action is taken and a successful response is given.
+	 *
+	 * This is an idempotent operation.
+	 *
+	 * @param attachment The attachment to migrate the data within
+	 * @return true if the contents of the attachment are now in the filesystem, regardless of action taken
+	 */
+	boolean migrateAttachmentFromDatabase(Attachment attachment){
+		boolean success = true
+		// If the attachment is present and not empty
+		if (!(attachment.content == null || attachment.content.length == 0)) {
+
+			// Does the target file exist already?
+			File targetFile = "".asType(File)
+			if (targetFile.exists()){
+				log.error( "The target file ${targetFile} already exists. No action taken.")
+				success = false
+			}
+			else {
+				// Create the file from the byte array in the same way as we do for real uploads
+				MockMultipartFile uploadFile = new MockMultipartFile(attachment.id as String, attachment.content)
+				String filePath = fileUploadService.uploadFile(uploadFile, attachment.id + ".jpg", 'attachments')
+				attachment.fileUrl = filePath
+
+				// Verify that the new file's contents are identical
+				File externalFile = attachment.fileUrl as File
+				if (!externalFile.exists() || externalFile.bytes != attachment.content) {
+					log.error("Created external file contents do not match original byte array. Aborting.")
+					success = false
+				}else{
+					log.info("Content migrated successfully for attachment ${attachment.id}")
+					// Remove the old data and clean up
+					attachment.content = null
+					attachment.save()
+				}
+			}
+		}
+		return success
+	}
 }
 
