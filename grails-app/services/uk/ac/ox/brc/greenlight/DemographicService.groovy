ac31a32952bd9d7941286a505761e572eb37a5f5
soheilsaif <s.saifipour@gmail.com>
2014-10-30 12:58:54 +0000

    Adding demographicService to retrieve patient demographic from EPDS through ODBC connection

--- grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
@@ -19,4 +19,12 @@ class DemographicService {
          //[[ORIGINAL_MRN:XXX, ACTIVE_MRN:xxxx, NHSNUMBER:9446457610, TITLE:MR, GIVENNAME:ABC, FAMILYNAME:AA, SEX:1, DOB:2010-05-17 00:00:00.0, DOD:null, ADDRESS_LINE1:ABCBCBCBC, ADDRESS_LINE3:AAAA, ADDRESS_LINE4:OXFORD, ADDRESS_LINE5:null, POSTCODE:OX3Q, ETHNICGROUP:C, GP_GMPCODE:G8703318, GP_PRACTICE_CODE:K84615]]
     }
 
+
+	/*
+	we can check if ODBC driver is supported
+	http://stackoverflow.com/questions/14229072/removal-of-jdbc-odbc-bridge-in-java-8
+ 	*/
+	def checkODBC(){
+		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+	}
 }
--- test/unit/uk/ac/ox/brc/greenlight/DemographicServiceSpec.groovy
+++ test/unit/uk/ac/ox/brc/greenlight/DemographicServiceSpec.groovy
@@ -1,6 +1,9 @@
 package uk.ac.ox.brc.greenlight
 
 import grails.test.mixin.TestFor
+import groovy.mock.interceptor.MockFor
+import groovy.sql.Sql
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
@@ -15,15 +18,50 @@ class DemographicServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "checkPatient"() {
-        when:"nhsNumber is passed"
-        def result = service.findPatient("9446457610")
 
-        then:"it returns a patient demographics"
-        result["ACTIVE_MRN"] == "10221601"
-        result["GIVENNAME"] == "THIRTYFIVECHARACTERLONGFORENAM"
-        result["FAMILYNAME"] == "FEATHERSTONEHAUGH-SMYTHE-WILLI"
-        result["SEX"] == "1"
-        result["DOB"].toString() == "2010-05-17 00:00:00.0"
- }
+	void "checkPatient"() {
+		expect: "returns null when passed null"
+		null == service.findPatient(null)
+
+		/**
+		 def mockSql = new MockFor(Sql.class)
+		 mockSql.demand.newInstance {def a,def b,def c,def d ->
+		 mockSql}mockSql.demand.findRow { def query,def param ->
+		 return "AAA"}def result
+		 mockSql.use {result = service.findPatient("9446457610")}* This mock approach doesn't work and I couldn't fix it!!
+		 * It seems that it doesn't work this way:
+		 * http://stackoverflow.com/questions/18825457/groovy-testing-groovy-sql-sql?answertab=active#tab-top
+		 * So I mocked Sql in an odd manner.
+		 */
+
+		def nhsNumber = "1234567890"
+		when: "nhs number is passed"
+		//Mock Sql newInstance
+		Sql.metaClass."static".newInstance = { String url, String username, String password, String driver ->
+			//mock its inner methods like findRow
+			return [
+					findRow: { def query, def params ->
+						//expect that findRow is called with proper params
+						assert query  == "select ACTIVE_MRN,GIVENNAME,FAMILYNAME,DOB,SEX from PMI.VW_COSD_STAGING_DB where NHSNUMBER=:nhsNum"
+						assert params == [nhsNum: nhsNumber]
+
+						//return as mocked result
+						[	ACTIVE_MRN: "10221601",
+								GIVENNAME: "ABCD",
+								FAMILYNAME: "HiABC",
+								SEX: "1",
+								DOB: new Date(2010, 05, 17)
+						]
+					}
+			]
+		}
+		def result = service.findPatient(nhsNumber)
+
+		then: "returns patient demographic"
+		result.ACTIVE_MRN == "10221601"
+		result.GIVENNAME  == "ABCD"
+		result.FAMILYNAME == "HiABC"
+		result.SEX == "1"
+		result.DOB == new Date(2010, 05, 17)
+	}
 }
