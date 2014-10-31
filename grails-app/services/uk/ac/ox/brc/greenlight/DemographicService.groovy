50186c1978d68cac441be6d526973ae4d20eb49a
soheilsaif <s.saifipour@gmail.com>
2014-10-31 14:03:25 +0000

    Fixing DemographicService mocking tests

--- grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
@@ -1,8 +1,6 @@
 package uk.ac.ox.brc.greenlight
-
 import groovy.sql.Sql
 import grails.transaction.Transactional
-import org.codehaus.groovy.grails.web.converters.configuration.configtest
 
 import java.sql.Connection
 import java.sql.DriverManager
@@ -16,20 +14,22 @@ class DemographicService {
 	 * Uses Oracle driver and connects Oracle 11.2.0 directly
 	 * we downloaded ojdbc6.jar and saved in in lib directory as the library source for Oracle Connectivity
 	 */
-
 	def findPatient(nhsNumber) {
 
 		if (!nhsNumber) {
 			return null
 		}
 
-		//ODBC Driver
-		//'sun.jdbc.odbc.JdbcOdbcDriver')
 		try {
 			def conString  = grailsApplication.config.epds.conString
 			Connection con = DriverManager.getConnection(conString.url, conString.username, conString.password);
 			def sql = new Sql(con);
-			sql.firstRow("select ACTIVE_MRN,GIVENNAME,FAMILYNAME,DOB,SEX from PMI.VW_COSD_STAGING_DB where NHSNUMBER=:nhsNum", [nhsNum: nhsNumber])
+			def row = sql.firstRow("select ACTIVE_MRN,GIVENNAME,FAMILYNAME,DOB,SEX from PMI.VW_COSD_STAGING_DB where NHSNUMBER=:nhsNum", [nhsNum: nhsNumber])
+			//as DOB is a Timestamp, its toString() will return the actual date in string format
+			//its month value is between [0-11]
+			def date  =  new Date().parse("yyyy-MM-dd",row.DOB.toString())
+			row.DOB = new Date(date[Calendar.YEAR],date[Calendar.MONTH],date[Calendar.DATE])
+			row
 			//[[ORIGINAL_MRN:XXX, ACTIVE_MRN:xxxx, NHSNUMBER:9446457610, TITLE:MR, GIVENNAME:ABC, FAMILYNAME:AA, SEX:1, DOB:2010-05-17 00:00:00.0, DOD:null, ADDRESS_LINE1:ABCBCBCBC, ADDRESS_LINE3:AAAA, ADDRESS_LINE4:OXFORD, ADDRESS_LINE5:null, POSTCODE:OX3Q, ETHNICGROUP:C, GP_GMPCODE:G8703318, GP_PRACTICE_CODE:K84615]]
 		}
 		catch(Exception ex){
--- test/unit/uk/ac/ox/brc/greenlight/DemographicServiceSpec.groovy
+++ test/unit/uk/ac/ox/brc/greenlight/DemographicServiceSpec.groovy
@@ -8,6 +8,8 @@ import spock.lang.Specification
 
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.Time
+import java.sql.Timestamp
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
@@ -15,14 +17,13 @@ import java.sql.DriverManager
 @TestFor(DemographicService)
 class DemographicServiceSpec extends Specification {
 
-    def setup() {
-    }
+	def setup() {
+	}
 
-    def cleanup() {
-    }
+	def cleanup() {
+	}
 
 
-	@Ignore
 	void "checkPatient"() {
 		expect: "returns null when passed null"
 		null == service.findPatient(null)
@@ -30,42 +31,46 @@ class DemographicServiceSpec extends Specification {
 		def nhsNumber = "1234567890"
 		when: "nhs number is passed"
 
-		def mock = new MockFor(Sql.class)
-		mock.demand.with {
-			Sql() { Connection con ->
-				//mock its inner methods like findRow
-				return [
-						firstRow: { def query, def params ->
-							//expect that findRow is called with proper params
-							assert query  == "select ACTIVE_MRN,GIVENNAME,FAMILYNAME,DOB,SEX from PMI.VW_COSD_STAGING_DB where NHSNUMBER=:nhsNum"
-							assert params == [nhsNum: nhsNumber]
+		DriverManager.metaClass."static".getConnection = { String url, String username, String password ->
+			return null;
+		};
 
-							//return as mocked result
-							[	ACTIVE_MRN: "10221601",
-									GIVENNAME: "ABCD",
-									FAMILYNAME: "HiABC",
-									SEX: "1",
-									DOB: new Date(2010, 05, 17)
-							]
-						}
-				]
-			}
+		Sql.metaClass.constructor = { Connection con ->
+			//mock its inner methods like firstRow
+			return [
+					firstRow: { def query, def params ->
+						//expect that findRow is called with proper params
+						assert query == "select ACTIVE_MRN,GIVENNAME,FAMILYNAME,DOB,SEX from PMI.VW_COSD_STAGING_DB where NHSNUMBER=:nhsNum"
+						assert params == [nhsNum: nhsNumber]
+
+						//return as mocked result
+						[ACTIVE_MRN: "10221601",
+								GIVENNAME: "John",
+								FAMILYNAME: "Smith",
+								SEX: "1",
+								/*
+								 * as the returned result from JDBC is a java.sql.Timestamp,
+								 * so if we create it for test we need to consider that, the constructor takes these:
+								 * http://docs.oracle.com/javase/7/docs/api/java/sql/Timestamp.html
+								 * year - the year minus 1900
+								 * month - 0 to 11
+								 * date - 1 to 31,....
+								 */
+								DOB: new Timestamp(2010 - 1900, 04, 17, 0, 0, 0, 0)
+						]
+					}
+			]
 		}
+
+		def result = service.findPatient(nhsNumber)
 
 		then: "returns patient demographic"
-		def result
-		mock.use {
-			result = service.findPatient(nhsNumber)
-		}
 		//it should just return these fields
 		result.size() == 5
 		result.ACTIVE_MRN == "10221601"
-		result.GIVENNAME  == "ABCD"
-		result.FAMILYNAME == "HiABC"
+		result.GIVENNAME == "John"
+		result.FAMILYNAME == "Smith"
 		result.SEX == "1"
-		result.DOB == new Date(2010, 05, 17)
+		result.DOB == new Date(2010, 04, 17)
 	}
-
-
-
 }
