6a039e6d2d5deef67a8e74948e181411eae1ec3d
soheilsaif <s.saifipour@gmail.com>
2014-11-05 11:22:32 +0000

    Fixes oracle.jdbc driver load into the app while deploying it into Tomcat

--- grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/DemographicService.groovy
@@ -4,11 +4,27 @@ import grails.transaction.Transactional
 
 import java.sql.Connection
 import java.sql.DriverManager
+//import oracle.jdbc.OracleDriver
 
 @Transactional
 class DemographicService {
 
 	def grailsApplication
+
+
+	//Tomcat can not find the oracle driver
+	//No suitable driver found for jdbc:oracle:thin:@...... when running web application
+	//It seems like the ojdbc6.jar should be placed into Tomcat CLASSPATH like the approach mentioned in the following
+	//http://stackoverflow.com/questions/6163447/including-external-jar-in-tomcat-classpath
+	//or instead of that we can add the following line and it will automatically load it into Tomcat
+	//http://stackoverflow.com/questions/13662475/no-suitable-driver-found-for-jdbcoraclethinlocalhost1521xe-when-running-we
+	static{
+		try {
+			Class.forName ("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/*
 	 * Uses Oracle driver and connects Oracle 11.2.0 directly
@@ -30,7 +46,7 @@ class DemographicService {
 			def date  =  new Date().parse("yyyy-MM-dd",row.DOB.toString())
 			row.DOB = new Date(date[Calendar.YEAR],date[Calendar.MONTH],date[Calendar.DATE])
 			row
-			//[[ORIGINAL_MRN:XXX, ACTIVE_MRN:xxxx, NHSNUMBER:9446457610, TITLE:MR, GIVENNAME:ABC, FAMILYNAME:AA, SEX:1, DOB:2010-05-17 00:00:00.0, DOD:null, ADDRESS_LINE1:ABCBCBCBC, ADDRESS_LINE3:AAAA, ADDRESS_LINE4:OXFORD, ADDRESS_LINE5:null, POSTCODE:OX3Q, ETHNICGROUP:C, GP_GMPCODE:G8703318, GP_PRACTICE_CODE:K84615]]
+			//[[ORIGINAL_MRN:XXX, ACTIVE_MRN:xxxx, NHSNUMBER:9446457610, TITLE:MR, GIVENNAME:ABC, FAMILYNAME:AA, SEX:1, DOB:2010-05-17 00:00:00.0, DOD:null, ADDRESS_LINE1:ABCBCBCBC, ADDRESS_LINE3:AAAA, ADDRESS_LINE4:OXFORD, ADDRESS_LINE5:null, POSTCODE:AAA, ETHNICGROUP:C, GP_GMPCODE:G8700000, GP_PRACTICE_CODE:K84615]]
 		}
 		catch(Exception ex){
 			log.error(ex)
