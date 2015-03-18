4ac9762e3e26ff7ffa0b58235553031a586712d9
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2015-03-18 14:43:45 +0000

    Adding more test for AttachmentController

--- test/unit/uk/ac/ox/brc/greenlight/AttachmentControllerSpec.groovy
+++ test/unit/uk/ac/ox/brc/greenlight/AttachmentControllerSpec.groovy
@@ -36,6 +36,10 @@ class AttachmentControllerSpec extends Specification {
 
 		then:"one attachment should be created"
 		1 * controller.attachmentService.create(_)  >> {new Attachment();}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments.each {
+			it.uploadStatus == "Success"
+		}
 	}
 
 
@@ -56,6 +60,10 @@ class AttachmentControllerSpec extends Specification {
 
 		then:"two attachments should be created"
 		2 * controller.attachmentService.create(_)  >> {new Attachment();}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments.each {
+			it.uploadStatus == "Success"
+		}
 	}
 
 
@@ -81,6 +89,8 @@ class AttachmentControllerSpec extends Specification {
 		then:"one attachment should be created"
 		1 * controller.PDFService.convertPDFToSingleImage(_,_) >> {}
 		1 * controller.attachmentService.create(_)  >> {new Attachment();}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments[0].uploadStatus == "Success"
 	}
 
 	def "save will create more than one attachments when singleConsentPerPDFFile is false and uploaded file is a multi-page PDF"(){
@@ -105,6 +115,122 @@ class AttachmentControllerSpec extends Specification {
 		then:"two attachments should be created and convertPDFToSingleImage should NOT be called"
 		0 * controller.PDFService.convertPDFToSingleImage(_,_) >> {}
 		2 * controller.attachmentService.create(_)  >> {new Attachment();}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments.each {
+			it.uploadStatus == "Success"
+		}
+	}
+
+	def "save will handle the error message if it is a multi-page PDF and singleConsentPerPDFFile is TRUE and convertPDFToSingleImage throws an exception"(){
+
+		given:"A large PDF is uploaded and singleConsentPerPDFFile is true"
+		//As we need to also mock the service which is used inside the controller, so we need to add the following line
+		controller.attachmentService = Mock(AttachmentService)
+		controller.PDFService = Mock(PDFService)
+
+		Path path = Paths.get("test/resources/multiPagePDF.pdf")
+		byte[] pdfContent = Files.readAllBytes(path)
+
+		def multipartFile = new GrailsMockMultipartFile('scannedForms', 'multiPagePDF.pdf', 'application/pdf', pdfContent)
+		controller.request.addFile(multipartFile)
+
+		//if all pages in the PDF belongs to the same consent
+		controller.params.singleConsentPerPDFFile = true
+
+		when:
+		controller.save()
+
+		then:"No attachment should be created"
+		1 * controller.PDFService.convertPDFToSingleImage(_,_) >> { throw new OutOfMemoryError("OutOfMemory in processing the PDF file")}
+		0 * controller.attachmentService.create(_)  >> {new Attachment();}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments[0].uploadStatus  == "Failed"
+		controller.modelAndView.model.attachments[0].uploadMessage == "OutOfMemory in processing the PDF file"
+		controller.modelAndView.model.attachments[0].fileName == "multiPagePDF.pdf"
+	}
+
+
+	def "save will handle the error message if it is a PDF and singleConsentPerPDFFile is FALSE and attachmentService throws an exception"(){
+
+		given:"A multi-page PDF is uploaded and singleConsentPerPDFFile is False"
+		//As we need to also mock the service which is used inside the controller, so we need to add the following line
+		controller.attachmentService = Mock(AttachmentService)
+		controller.PDFService = Mock(PDFService)
+
+		Path path = Paths.get("test/resources/multiPagePDF.pdf")
+		byte[] pdfContent = Files.readAllBytes(path)
+
+		def multipartFile = new GrailsMockMultipartFile('scannedForms', 'multiPagePDF.pdf', 'application/pdf', pdfContent)
+		controller.request.addFile(multipartFile)
+
+		//if all pages in the PDF belongs to the same consent
+		controller.params.singleConsentPerPDFFile = false
+
+		when:
+		controller.save()
+
+		then:"No attachment should be created"
+		2 * controller.attachmentService.create(_)  >> {throw new Exception("Exception in creating the attachment");}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments.eachWithIndex  { Attachment attachment, int index ->
+			attachment.uploadStatus  == "Failed"
+			attachment.uploadMessage == "Exception in creating the attachment"
+			attachment.fileName == "multiPagePDF.pdf[Page:${index+1}]"
+		}
+
+	}
+
+	def "save will handle the error message if it is a non PDF and attachmentService throws an exception\""(){
+
+		given:"JPG file is uploaded"
+		//As we need to also mock the service which is used inside the controller, so we need to add the following line
+		controller.attachmentService = Mock(AttachmentService)
+
+		Path path = Paths.get("test/resources/singlePageJPG.jpg")
+		byte[] jpgFile = Files.readAllBytes(path)
+
+		def multipartFile = new GrailsMockMultipartFile('scannedForms', 'singlePageJPG.jpg', 'image/jpg', jpgFile)
+		controller.request.addFile(multipartFile)
+
+		when:
+		controller.save()
+
+		then:"no attachment should be created"
+		1 * controller.attachmentService.create(_)  >> {throw new Exception("Exception in creating the attachment");}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments[0].uploadStatus  == "Failed"
+		controller.modelAndView.model.attachments[0].uploadMessage == "Exception in creating the attachment"
+		controller.modelAndView.model.attachments[0].fileName == "singlePageJPG.jpg"
+	}
+
+	def "save will handle the error message if it is a corrupted PDF file!"(){
+
+		given:"A multi-page PDF is uploaded and singleConsentPerPDFFile is False"
+		//As we need to also mock the service which is used inside the controller, so we need to add the following line
+		controller.attachmentService = Mock(AttachmentService)
+		controller.PDFService = Mock(PDFService)
+
+		Path path = Paths.get("test/resources/corruptedPDFFile.pdf")
+		byte[] pdfContent = Files.readAllBytes(path)
+
+		def multipartFile = new GrailsMockMultipartFile('scannedForms', 'multiPagePDF.pdf', 'application/pdf', pdfContent)
+		controller.request.addFile(multipartFile)
+
+		//if all pages in the PDF belongs to the same consent
+		controller.params.singleConsentPerPDFFile = false
+
+		when:
+		controller.save()
+
+		then:"No attachment should be created"
+		0 * controller.attachmentService.create(_)  >> {}
+		controller.modelAndView.model.attachments
+		controller.modelAndView.model.attachments.eachWithIndex  { Attachment attachment, int index ->
+			attachment.uploadStatus  == "Failed"
+			attachment.uploadMessage == "Can not read the PDF file!"
+			attachment.fileName == "corruptedPDFFile.pdf"
+		}
+
 	}
 
 }
