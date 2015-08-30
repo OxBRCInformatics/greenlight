7a924a4c043b82732ea51ceb6e92d64c51908f5f
soheil.saifipour <soheil.saifipour@ndm.ox.ac.uk>
2015-08-30 13:14:36 +0100

    Adding test for CDRService

--- test/unit/uk/ac/ox/brc/greenlight/CDRServiceSpec.groovy
+++ test/unit/uk/ac/ox/brc/greenlight/CDRServiceSpec.groovy
@@ -1,5 +1,6 @@
 package uk.ac.ox.brc.greenlight
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +8,29 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(CDRService)
+@Mock([ConsentForm,Patient])
 class CDRServiceSpec extends Specification {
 
-	def setup() {
-	}
 
-	def cleanup() {
-	}
 
-	void "test something"() {
+
+
+	void "saveOrUpdateConsentForm saves new consentForm in CDR"() {
+
+		setup:
+		def sendConsentToCDRCalled = false
+		service.metaClass.sendConsentToCDR = { nhsNumber,hospitalNumber,consent ->
+			sendConsentToCDRCalled = true
+			return "success"
+		}
+
+		when:
+		def patient = new Patient()
+		def consentForm = new ConsentForm()
+		def result = service.saveOrUpdateConsentForm(patient,consentForm)
+
+		then:
+		sendConsentToCDRCalled
+		result == "success"
 	}
 }
