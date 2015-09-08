aeed0c606921694760dc37cb9f3a55d7df1b4bc0
soheil.saifipour <soheil.saifipour@ndm.ox.ac.uk>
2015-09-08 16:38:20 +0100

    Refactoring CDRLog class and adding new fields

--- grails-app/domain/uk/ac/ox/brc/greenlight/Audit/CDRLog.groovy
+++ grails-app/domain/uk/ac/ox/brc/greenlight/Audit/CDRLog.groovy
@@ -1,16 +1,40 @@
 package uk.ac.ox.brc.greenlight.Audit
 
+import uk.ac.ox.brc.greenlight.ConsentForm
+
 class CDRLog {
 
-	String consentId
+	String consentFormId
 	String consentTemplateId
+	Date consentDate
+	ConsentForm.ConsentStatus consentStatus
+	String comment
+	String consentStatusLabels
+	String cdrUniqueId
+	String namePrefix
+	String consentURL
+	String consentAccessGUID
+
 	String nhsNumber
 	String hospitalNumber
+
+	Date actionDate
 	String action
-	String result
+	boolean persistedInCDR
 	String resultDetail
 
 	static constraints = {
+
+		consentFormId nullable:true
+		consentTemplateId nullable:true
+
+		comment type:"text" , nullable:true
+		consentStatusLabels nullable: true
+
+		nhsNumber nullable:true
+		hospitalNumber nullable:true
+
 		resultDetail type: "text"
+		consentStatusLabels type:"text"
 	}
 }
