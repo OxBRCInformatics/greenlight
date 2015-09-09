b85493b5ac9663d528605358fac1843128bdf637
soheil.saifipour <soheil.saifipour@ndm.ox.ac.uk>
2015-09-09 09:35:11 +0100

    Passing new parameters into the add log method

--- grails-app/services/uk/ac/ox/brc/greenlight/CDRLogService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/CDRLogService.groovy
@@ -5,4 +5,25 @@ import uk.ac.ox.brc.greenlight.Audit.CDRLog
 
 class CDRLogService {
 
+	def add(String nhsNumber,String hospitalNumber,Map consentDetailsMap,boolean persistedInCDR,String resultDetail,String actionType) {
+		new CDRLog(
+				consentFormId: consentDetailsMap?.consentFormId,
+				consentAccessGUID: consentDetailsMap?.consentAccessGUID,
+				consentTemplateId: consentDetailsMap?.consentTemplateId,
+				consentDate:consentDetailsMap?.consentDate,
+				consentStatus:consentDetailsMap?.consentStatus,
+				comment:consentDetailsMap?.comment,
+				consentStatusLabels:consentDetailsMap?.consentStatusLabels,
+				cdrUniqueId:consentDetailsMap?.cdrUniqueId,
+				namePrefix:consentDetailsMap?.namePrefix,
+				consentURL:consentDetailsMap?.consentURL,
+
+				nhsNumber: nhsNumber,
+				hospitalNumber: hospitalNumber,
+
+				actionDate: new Date(),
+				action: actionType,
+				persistedInCDR: persistedInCDR,
+				resultDetail: resultDetail).save(flush: true,failOnError: true)
+	}
 }
