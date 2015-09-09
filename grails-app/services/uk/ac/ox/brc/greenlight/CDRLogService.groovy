91f0fdf2ac72d679024e815ba42e7b79d0c0e1af
soheil.saifipour <soheil.saifipour@ndm.ox.ac.uk>
2015-09-09 09:34:40 +0100

    Removing the log add method

--- grails-app/services/uk/ac/ox/brc/greenlight/CDRLogService.groovy
+++ grails-app/services/uk/ac/ox/brc/greenlight/CDRLogService.groovy
@@ -5,14 +5,4 @@ import uk.ac.ox.brc.greenlight.Audit.CDRLog
 
 class CDRLogService {
 
-	def add(consentFormId, consentTemplateId, nhsNumber, hospitalNumber, result, resultDetail,actionType) {
-		new CDRLog(
-				consentId: consentFormId,
-				consentTemplateId: consentTemplateId,
-				nhsNumber: nhsNumber,
-				hospitalNumber: hospitalNumber,
-				action: actionType,
-				result: result,
-				resultDetail: resultDetail).save(flush: true)
-	}
 }
